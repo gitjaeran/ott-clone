8602adfb705c98a26fd3e18d070da5817b0ca217
JaeranKIM <devjaeran@gmail.com>
2023-09-10 16:31:06 +0900

    home screen - banner, scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Header from "./Components/Header";
 import Home from "./Routes/Home";
 import Search from "./Routes/Search";
 import Tv from "./Routes/Tv";
 
 function App() {
+  const client = new QueryClient();
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/tv" element={<Tv />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={client}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/tv" element={<Tv />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -61,13 +61,16 @@ table {
 body {
   font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
-  color:black;
+  color:${props => props.theme.white.darker};
   line-height: 1.2;
-  
+  background-color: black;
 }
 a {
   text-decoration:none;
   color:inherit;
+}
+::-webkit-scrollbar {
+display: none;
 }
 `;
 
